c73ce808f6498018cdeaf7760d2443d8c626828f
noranuko13 <noranuko13@gmail.com>
2021-08-17 09:55:20 +0900

    :notes: [Tutorial] Resolve duplicates: indexOfWinner

--- src/Services/Decision/index.tsx
+++ src/Services/Decision/index.tsx
@@ -1,23 +1,28 @@
 import { LINES } from '../../constants'
 
-export const calculateWinner = (squares: string[]): string => {
+const indexOfWinner = (squares: string[]): number => {
   for (let i = 0; i < LINES.length; i++) {
     const [a, b, c] = LINES[i]
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
+      return i
     }
   }
 
+  return -1
+}
+
+export const calculateWinner = (squares: string[]): string => {
+  const index = indexOfWinner(squares)
+  if (index !== -1) {
+    return squares[LINES[index][0]]
+  }
   return ''
 }
 
 export const calculateLine = (squares: string[]): ReadonlyArray<number> => {
-  for (let i = 0; i < LINES.length; i++) {
-    const [a, b, c] = LINES[i]
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return LINES[i]
-    }
+  const index = indexOfWinner(squares)
+  if (index !== -1) {
+    return LINES[index]
   }
-
   return []
 }
