eb23816e12a7826fc2d2c510e0f20e4288d6cea8
noranuko13 <noranuko13@gmail.com>
2023-06-25 20:39:13 +0900

    :notes: [JetBrains] Promise returned from init is ignored

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,14 +2,17 @@ import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import enTranslation from './resources/locales/en/translation.json'
 
-i18next.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: enTranslation,
+i18next
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
     },
-  },
-  lng: 'en',
-  interpolation: {
-    escapeValue: false,
-  },
-})
+    lng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+  .then()
