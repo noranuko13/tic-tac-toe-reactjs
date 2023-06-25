4ac87dbd0245aff84631abe43f78160cda2909c5
noranuko13 <noranuko13@gmail.com>
2023-06-25 14:26:43 +0900

    :notes: [Prettier] Add prettier-ignore

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import reportWebVitals from './reportWebVitals'
 import { Game } from './Domains/Game'
 
+// prettier-ignore
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
