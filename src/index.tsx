f167b7da7a443f4c03c19481ad13e01678eae8e5
noranuko13 <noranuko13@gmail.com>
2023-07-05 20:36:21 +0900

    :notes: [Sass] Change extensions

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './index.scss'
 import reportWebVitals from './reportWebVitals'
 import { I18nextProvider } from 'react-i18next'
 import { i18next } from './i18n'
