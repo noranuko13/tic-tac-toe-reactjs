b4d22a027a2314ac97db82cfaf115525ea764fe8
noranuko13 <noranuko13@gmail.com>
2025-05-15 18:32:02 +0900

    :notes: [npm-check-updates] 2025-05-15 v18.0.1 - Web Vitals

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,12 +1,12 @@
-import { ReportCallback } from 'web-vitals'
+import { MetricType } from 'web-vitals'
 
-const reportWebVitals = (onPerfEntry?: ReportCallback) => {
+const reportWebVitals = (onPerfEntry?: (metric: MetricType) => void) => {
   // noinspection SuspiciousTypeOfGuard
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
+    import('web-vitals').then(({ onCLS, onFCP, onINP, onLCP, onTTFB }) => {
       onCLS(onPerfEntry)
-      onFID(onPerfEntry)
       onFCP(onPerfEntry)
+      onINP(onPerfEntry)
       onLCP(onPerfEntry)
       onTTFB(onPerfEntry)
     })
